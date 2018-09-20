c04fb0086e0c31d7eb7912d2d7a3eec12c6a7158
Paul Fell <swellpf@gmail.com>
2018-09-20 15:03:19 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
-def unsafe?(speed)
+# require 'pry'
 
+def unsafe?(speed)
+ # binding.pry
+  if speed < 40 || speed > 60
+    return TRUE
+  else
+    return FALSE
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+#	binding.pry
+	speed < 40 || speed > 60 ? return TRUE:return FALSE
 end
 	
 
